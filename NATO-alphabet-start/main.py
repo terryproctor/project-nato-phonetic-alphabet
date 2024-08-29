c084213b499990e92a38fe2fe5a980fb8e5d9462
terryproctor <terryproctor2015@outlook.com>
2024-08-29 22:42:06 +0100

    now has try and catch key exceptions

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 #TODO 1. Create a dictionary in this format:
 #{"A": "Alfa", "B": "Bravo"}
 
@@ -6,7 +8,20 @@ nato_df = pd.read_csv(nato)
 nato_dict = {row.letter : row.code for (index, row) in nato_df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-words = input("Enter word to convert from:\n")
+def gen_phonetic():
+    phonetics = []
+    while len(phonetics) == 0: 
+        try:
+            words = input("Enter word to convert from:\n")
+            phonetics = [nato_dict[letter.upper()] for letter in words if letter.upper()]
+        except KeyError:
+            print("Sorry, only letters in the alphabet please")
+        else:
+            print(phonetics)
+            return 
 
-phonetics = [nato_dict[letter.upper()] for letter in words if letter.upper() in nato_dict.keys()]
-print(phonetics)
+gen_phonetic()
+
+    
+
+
